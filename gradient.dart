07d5526f66c93d9bdf1a70159927244397f4ee08
Euclid Stellar <euclidstellar@gmail.com>
2023-06-15 22:15:36 +0530

    variable practice added

--- gradient.dart
+++ gradient.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:rolldice/style.dart';
 
+const startkidharse = Alignment.bottomLeft;
+const endkidharkarnahai = Alignment.topRight;
+const transformkidharse = GradientRotation(23);
+
 class GradientMix extends StatelessWidget {
   const GradientMix({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          //Color.fromARGB(232, 239, 73, 225),
-          Color.fromARGB(233, 255, 255, 255),
-          Color.fromARGB(225, 119, 234, 74),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            //Color.fromARGB(232, 239, 73, 225),
+            Color.fromARGB(233, 255, 255, 255),
+            Color.fromARGB(225, 119, 234, 74),
 
-          // Color.fromARGB(232, 239, 73, 225),
-        ],
-        begin: Alignment.bottomLeft,
-        transform: GradientRotation(23),
-        end: Alignment.topRight,
-      )),
-      child:const StyleText()
-    );
+            // Color.fromARGB(232, 239, 73, 225),
+          ],
+          // begin: Alignment.bottomLeft,
+          // transform: GradientRotation(23),
+          // end: Alignment.topRight,
+
+          begin: startkidharse,
+          transform: transformkidharse,
+          end: endkidharkarnahai,
+        )),
+        child: const StyleText());
   }
 }
